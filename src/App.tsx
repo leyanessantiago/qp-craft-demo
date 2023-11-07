384ec076f759f230f7d449ad5c621250296f136c
leyanessantiago <le.yanessantiago@gmail.com>
2023-11-08 01:15:39 +0200

    add Featured Card select

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useMemo, useState } from 'react';
 import Parks from './components/Parks/Parks';
+import Select, { OptionValue } from './components/base/Select/Select';
+import { parks } from './data/dataset1.json'
 import './App.scss'
 
 function App() {
+  const [featuredCard, setFeaturedCard] = useState('');
+  const parksWithImage = useMemo(() => parks.filter((item) => !!item.image), [])
+
+  const options: OptionValue[] = useMemo(() =>
+    parksWithImage.map((item, index) => ({ label: item.title || 'National Park', value: index.toString() })),
+    [parksWithImage]
+  );
+
+  const handleSelectChange = (newValue: string) => {
+    setFeaturedCard(newValue);
+  };
+
   return (
     <main>
-      <Parks />
+      <Select
+        id='featured-card'
+        label='Featured Card'
+        options={options}
+        onSelect={handleSelectChange}
+        value={featuredCard}
+        placeholder='Select Featured Card'
+      />
+      <Parks featuredCard={featuredCard} />
     </main>
   )
 }
